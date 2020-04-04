9c5924a788a794d49ecf9392f7cf66b640819014
Charan <sreecharan999@hotmail.com>
2020-04-04 06:18:35 +0400

    commented out the js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,10 @@
+// Selecting the zoom in scale of the main wrapper
 var tlFirstScroll = new TimelineMax();
-
 
 tlFirstScroll
 .set('.iphone-image-wrapper', {scale: 3, transformOrigin:"center top"})
 .to('.iphone-image-wrapper', 3, {scale: 2.2, y: '-50%'})
 .to('.iphone-image-wrapper', 3, {scale: 1, y: '0%'})
-
-
 
 var controller = new ScrollMagic.Controller();
 
@@ -25,24 +23,27 @@ var scene1 = new ScrollMagic.Scene({
 .addTo(controller);
 
 
+// Image replacing
 var tlSecondScroll = new TimelineMax();
-
+// moving the images to left and right
 tlSecondScroll
 .to(".iphone1", 3, {x: "-54%"})
 .to(".iphone2", 3, {x: "54%"}, "-=3")
+// adding opacity 0 to text
 .from(".iphone1-text", 0.3,{autoAlpha: 0}, "-=3")
 .from(".iphone2-text", 0.3,{autoAlpha: 0}, "-=3")
+// at the same time moving the text along 
 .to(".iphone1-text", 3,{x:"-30%"}, "-=3")
 .to(".iphone2-text", 3,{x:"30%"}, "-=3")
-
+// changing position abosulte image property to display block
 .set(".iphone-stick", {display: "block"})
-
+// showing the images
 .to(".iphone1-img-behind", 3, {x: "-50%"})
 .to(".iphone2-img-behind", 3, {x: "50%"}, "-=3")
-
+// hiding the images
 .to(".iphone1-img", 0.5, {autoAlpha: 0}, "-=3")
 .to(".iphone2-img", 0.3, {autoAlpha: 0}, "-=3")
-
+// at the sane the text
 .to(".iphone1-text", 0.3, {autoAlpha: 0},"-=3")
 .to(".iphone2-text", 0.3, {autoAlpha: 0}, "-=3")
 
